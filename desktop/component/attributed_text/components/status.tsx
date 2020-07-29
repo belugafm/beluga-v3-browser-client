ad8af822e401b5616e839a1c27219f534210cd1b
belugafm <13643825+belugafm@users.noreply.github.com>
2020-07-29 14:11:03 +0900

    memo

--- desktop/component/attributed_text/components/status.tsx
+++ desktop/component/attributed_text/components/status.tsx
@@ -1,34 +1,43 @@
+import React from "react"
 import LinkTextComponent from "./link"
 import { StatusObjectT } from "../../../api/object"
 
-type PropTypes = {
+type Props = {
     status: StatusObjectT
     url: string
     showOriginalUrl: boolean
     callback: (status: StatusObjectT) => Promise<void>
 }
 
-export default (props: PropTypes) => {
-    const { url, status, showOriginalUrl, callback } = props
-    return (
-        <>
-            <LinkTextComponent
-                href={url}
-                hidden={!showOriginalUrl}
-                onClick={() => {
-                    callback(status)
-                }}
-            />
-            <div>
-                <span>{status.text}</span>
-            </div>
-            <style jsx>{`
-                div {
-                    border: 1px solid black;
-                    border-radius: 6px;
-                    padding: 6px;
-                }
-            `}</style>
-        </>
-    )
-}
+export default React.memo(
+    (props: Props) => {
+        const { url, status, showOriginalUrl, callback } = props
+        return (
+            <>
+                <LinkTextComponent
+                    href={url}
+                    hidden={!showOriginalUrl}
+                    onClick={() => {
+                        callback(status)
+                    }}
+                />
+                <div>
+                    <span>{status.text}</span>
+                </div>
+                <style jsx>{`
+                    div {
+                        border: 1px solid black;
+                        border-radius: 6px;
+                        padding: 6px;
+                    }
+                `}</style>
+            </>
+        )
+    },
+    (prevProps: Props, nextProps: Props) => {
+        if (prevProps.status.text !== nextProps.status.text) {
+            return false
+        }
+        return true
+    }
+)

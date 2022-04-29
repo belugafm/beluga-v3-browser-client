b98bf4e74285bb5c411da0314690365c4e911bbb
belugafm <13643825+belugafm@users.noreply.github.com>
2022-04-29 16:33:01 +0900

    fix random

--- desktop/component/chat/message/avatar.tsx
+++ desktop/component/chat/message/avatar.tsx
@@ -4,34 +4,51 @@ import { UserObjectT } from "../../../api/object"
 type PropsT = {
     user: UserObjectT
 }
+
 class Random {
     x: number
     y: number
     z: number
     w: number
     constructor(seed: number) {
-        this.x = 123456789
-        this.y = 362436069
-        this.z = 521288629
-        this.w = seed
+        seed ^= seed << 13
+        seed ^= 2 >>> 17
+        seed ^= seed << 5
+        this.x = 123456789 ^ seed
+        seed ^= seed << 13
+        seed ^= 2 >>> 17
+        seed ^= seed << 5
+        this.y = 362436069 ^ seed
+        seed ^= seed << 13
+        seed ^= 2 >>> 17
+        seed ^= seed << 5
+        this.z = 521288629 ^ seed
+        seed ^= seed << 13
+        seed ^= 2 >>> 17
+        seed ^= seed << 5
+        this.w = 88675123 ^ seed
     }
-
-    next(min: number, max: number) {
+    next() {
         let t
         t = this.x ^ (this.x << 11)
         this.x = this.y
         this.y = this.z
         this.z = this.w
-        const n = (this.w = this.w ^ (this.w >>> 19) ^ (t ^ (t >>> 8)))
+        // >>>0 means 'cast to uint32'
+        this.w = (this.w ^ (this.w >>> 19) ^ (t ^ (t >>> 8))) >>> 0
+        return this.w / 0x100000000
+    }
+    integer(min: number, max: number) {
+        const n = this.next()
         const r = Math.abs(n)
         return min + (r % (max + 1 - min))
     }
 }
 const DefaultAvatarComponent = ({ user }: PropsT) => {
     const gen = new Random(user.id)
-    const hue = gen.next(0, 360)
-    const sat = 60
-    const lightness = 60
+    const hue = 360 * gen.next()
+    const sat = 50
+    const lightness = 50
     return (
         <>
             <div className="avatar"></div>
@@ -58,7 +75,7 @@ const DefaultAvatarComponent = ({ user }: PropsT) => {
 export const MessageAvatarComponent = React.memo(
     (props: PropsT) => {
         const { user } = props
-        console.info("MessageAvatarComponent::render", user.profile_image_url)
+        console.info("MessageAvatarComponent::render", user.id, user.profile_image_url)
         if (user.profile_image_url == null) {
             return <DefaultAvatarComponent user={user} />
         }

fa1fa93981825be58ee6e18759b29c09e8111406
belugafm <13643825+belugafm@users.noreply.github.com>
2022-10-19 22:33:14 +0900

    add device to server side props

--- desktop/component/chat/next.ts
+++ desktop/component/chat/next.ts
@@ -1,9 +1,33 @@
 import Cookie from "cookie"
 import { GetServerSideProps } from "next"
+import { isMobile, isTablet } from "react-device-detect"
+import { isString } from "../../lib/type_check"
+import { Device } from "./next.types"
+
+const getDevice = (prefer_device: string | null) => {
+    if (isString(prefer_device)) {
+        if (prefer_device == Device.Desktop) {
+            return Device.Desktop
+        }
+        if (prefer_device == Device.Mobile) {
+            return Device.Mobile
+        }
+        if (prefer_device == Device.Tablet) {
+            return Device.Tablet
+        }
+    }
+    if (isTablet) {
+        return Device.Tablet
+    }
+    if (isMobile) {
+        return Device.Mobile
+    }
+    return Device.Desktop
+}
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const cookie = Cookie.parse(context.req.headers.cookie || "")
-    const { theme, user_name } = cookie
+    const { theme, user_name, prefer_device } = cookie
     const { params, query } = context
     return {
         props: {
@@ -11,6 +35,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
             user_name: user_name ? user_name : null,
             params: params ? params : {},
             query: query ? query : {},
+            device: getDevice(prefer_device),
         },
     }
 }
---
+++ desktop/component/chat/next.types.ts
@@ -0,0 +1,5 @@
+export const Device = {
+    Mobile: "mobile",
+    Desktop: "desktop",
+    Tablet: "tablet",
+}

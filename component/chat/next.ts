11d05bfc341191b55150977220bfce5155cd1a9b
belugafm <13643825+belugafm@users.noreply.github.com>
2022-10-20 20:21:07 +0900

    set device cookie

--- component/chat/next.ts
+++ component/chat/next.ts
@@ -3,16 +3,34 @@ import { GetServerSideProps } from "next"
 import { isMobile, isTablet } from "react-device-detect"
 import { isString } from "../../lib/type_check"
 import { Device } from "./next.types"
+import { setCookie } from "cookies-next"
+import config from "../../config"
+import nookies from "nookies"
 
-const getDevice = (prefer_device: string | null) => {
-    if (isString(prefer_device)) {
-        if (prefer_device == Device.Desktop) {
+const isValidDeviceString = (device: any) => {
+    if (isString(device)) {
+        if (device == Device.Desktop) {
+            return true
+        }
+        if (device == Device.Mobile) {
+            return true
+        }
+        if (device == Device.Tablet) {
+            return true
+        }
+    }
+    return false
+}
+
+const getDevice = (cookie_device: any) => {
+    if (isString(cookie_device)) {
+        if (cookie_device == Device.Desktop) {
             return Device.Desktop
         }
-        if (prefer_device == Device.Mobile) {
+        if (cookie_device == Device.Mobile) {
             return Device.Mobile
         }
-        if (prefer_device == Device.Tablet) {
+        if (cookie_device == Device.Tablet) {
             return Device.Tablet
         }
     }
@@ -33,17 +51,52 @@ export type ServerSideProps = {
     device: string
 }
 
+const buildQueryString = (query: { [key: string]: string }, exclude_keys: string[]): string => {
+    const pairs = []
+    Object.keys(query).forEach((key) => {
+        if (exclude_keys.includes(key)) {
+            return
+        }
+        const value = query[key]
+        pairs.push(`${key}=${value}`)
+    })
+    if (pairs.length == 0) {
+        return ""
+    }
+    return pairs.join("&")
+}
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const cookie = Cookie.parse(context.req.headers.cookie || "")
-    const { theme, user_name, prefer_device } = cookie
+    const { theme, user_name } = cookie
     const { params, query } = context
+    const device = query.device ? query.device : cookie.device
+    if (isValidDeviceString(query.device)) {
+        // @ts-ignore
+        nookies.set(context, "device", query.device, {
+            maxAge: 360 * 24 * 60 * 60,
+            httpOnly: !config.server.https,
+            secure: config.server.https,
+            path: "/",
+            domain: config.server.domain,
+        })
+        // @ts-ignore
+        const queryStr = buildQueryString(query, ["device"])
+        const baseUrl = context.resolvedUrl.replace(/\?.*$/, "")
+        return {
+            redirect: {
+                permanent: false,
+                destination: queryStr.length == 0 ? baseUrl : baseUrl + "?" + queryStr,
+            },
+        }
+    }
     return {
         props: {
             theme: theme ? theme : null,
             userName: user_name ? user_name : null,
             params: params ? params : {},
             query: query ? query : {},
-            device: getDevice(prefer_device),
+            device: getDevice(device),
         },
     }
 }

96d1d2d32d8beecd18c9fd2ce6365af541154fd7
belugafm <13643825+belugafm@users.noreply.github.com>
2023-03-12 18:15:51 +0900

    fix margin of default avatar

--- component/desktop/chat/message/Reply.tsx
+++ component/desktop/chat/message/Reply.tsx
@@ -47,7 +47,7 @@ const RandomColorComponent = ({ user }: { user: UserObjectT }) => {
                     mask-size: 100% 100%;
                     -webkit-mask-image: url('data:image/svg+xml;utf8,<svg preserveAspectRatio="none" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"><path d="M 0, 100 C 0, 23 23, 0 100, 0 S 200, 23 200, 100 177, 200 100, 200 0, 177 0, 100" fill="white"></path></svg>');
                     border-radius: 0;
-                    margin-top: 2px;
+                    margin: 0 8px;
                 }
             `}</style>
             <style jsx>{`
@@ -140,7 +140,9 @@ export const ReplyComponent = ({
                     text-overflow: ellipsis;
                 }
                 svg {
-                    flex: 0 0 24px;
+                    flex: 0 0 auto;
+                    width: 22px;
+                    height: 22px;
                     padding: 0 6px;
                     stroke: ${getStyle(theme)["stroke"]};
                 }
@@ -152,9 +154,10 @@ export const ReplyComponent = ({
                     color: ${getStyle(theme)["primaryColor"]};
                 }
                 .count {
+                    font-size: 12px;
                     flex: 0 0 auto;
                     color: ${getStyle(theme)["secondaryColor"]};
-                    margin-left: 10px;
+                    margin-left: 6px;
                 }
                 a:hover {
                     background-color: ${getStyle(theme)["hoverBgColor"]};
